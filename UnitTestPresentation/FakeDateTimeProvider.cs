e2a812eddc8d19e0cd7c2745f8c30e771170d988
Martin Dennhardt <martin.dennhardt@ituc.de>
2018-06-15 12:02:35 +0200

    Add simple fake IDateTimeProvider
    If you ever want to set your own date.

---
+++ UnitTestPresentation/FakeDateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnitTestPresentation.Library;
+
+namespace UnitTestPresentation
+{
+    public class FakeDateTimeProvider : IDateTimeProvider
+    {
+        public DateTime DateTime { get; set; } = new DateTime();
+
+        public DateTime GetCurrentDateTime()
+        {
+            return DateTime;
+        }
+    }
+}
