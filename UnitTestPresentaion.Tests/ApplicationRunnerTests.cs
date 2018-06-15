da6ed3af8bfa0d58c4af402cf8b17eb5e29e32e7
Martin Dennhardt <martin.dennhardt@ituc.de>
2018-06-15 12:02:35 +0200

    Tests for ApplicationRunner

---
+++ UnitTestPresentaion.Tests/ApplicationRunnerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using UnitTestPresentation.Library;
+
+namespace UnitTestPresentation.Tests
+{
+    [TestFixture]
+    public class ApplicationRunnerTests
+    {
+        private IDateTimeProvider _dateTimeProviderStub;
+        private IOutputHandler _outputHandlerMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dateTimeProviderStub = Substitute.For<IDateTimeProvider>();
+            _outputHandlerMock = Substitute.For<IOutputHandler>();
+        }
+
+        [Test]
+        public void ShowWelcomeMessage_ItsChristmas_ShowChristmasMessage()
+        {
+            // Arrange
+            _dateTimeProviderStub.GetCurrentDateTime().Returns(new DateTime(2018, 12, 25));
+
+            // Act
+            new ApplicationRunner(_dateTimeProviderStub, _outputHandlerMock).ShowWelcomeMessage();
+
+            // Assert
+            _outputHandlerMock.Received(1).WriteLine("Frohe Weihnachten!");
+        }
+
+        [Test]
+        public void ShowWelcomeMessage_ItsNotChristmas_ShowDefaultMessage()
+        {
+            // Arrange
+            _dateTimeProviderStub.GetCurrentDateTime().Returns(new DateTime(2018, 12, 24));
+
+            // Act
+            new ApplicationRunner(_dateTimeProviderStub, _outputHandlerMock).ShowWelcomeMessage();
+
+            // Assert
+            _outputHandlerMock.Received(1).WriteLine("Willkommen!");
+        }
+    }
+}
